16318c4485f526da700a87d90a882e4c6c4d29ce
raotarun <42333717+raotarun@users.noreply.github.com>
2018-08-18 01:18:29 +0530

    file

---
+++ ass1.py
@@ -0,0 +1,34 @@
+#Print anything you want on screen.
+a=100
+print(a)
+
+#Join two strings using '+'.  E.g.-"Acad"+"View”
+a=input("enter first string")
+b=input("enter second string")
+c=a+b
+print(c)
+
+# Take the input of 3 variables x, y and z . Print their values on screen.
+
+x=input("First String")
+y=input("Second String")
+z=input("Third String")
+print(x,y,z)
+
+#Print “Let’s get started” on screen.
+print('''“Let’s get started"''')
+
+#Print the following values using placeholders. s=”Acadview” course=”Python” fees=500
+s='Acadview' 
+course='Python'
+fees=500
+print('%s  %d  %s' %(s,fees,course))
+
+# Find the area of circle pi = 3.14 Take radius as input from user Print the area of circle
+pi=3.14
+r=int(input("Enter radius"))
+print("Area of circle is",pi*r*r)
+
+
+
+
